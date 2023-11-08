55a65bc70c9b5d0a806c9186d04a69fce6ce1400
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-11-08 21:41:55 +0800

    Update Linux.bat

--- RuleGen/Linux.bat
+++ RuleGen/Linux.bat
@@ -1,2 +1,3 @@
-cat route4.txt | gawk '{print $1,$2}' > route4.temp
+cat route4.temp | gawk '{print $1,$2}' > route4.txt
 
+cat route6.temp | gawk '{print $3}' | grep -v 'fe80' | grep -v 'ff00' | grep -v '/0' | grep -v '::1/128' > route6.txt
